c2439af90d57609f526a8a57a471d34a863bd668
Aleksey Krichevskiy <boolivar@gmail.com>
2026-01-04 00:33:56 +0200

    feat: --force and regex options

--- agent/src/main/java/org/bool/tostring/agent/ToStringAgent.java
+++ agent/src/main/java/org/bool/tostring/agent/ToStringAgent.java
@@ -10,27 +10,33 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-import static net.bytebuddy.matcher.ElementMatchers.declaresMethod;
-import static net.bytebuddy.matcher.ElementMatchers.isToString;
-import static net.bytebuddy.matcher.ElementMatchers.not;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class ToStringAgent {
 
     public static void premain(String agentArgs, Instrumentation i13n) {
-        new ToStringAgent().install(i13n);
+        List<String> options = Arrays.stream(Objects.toString(agentArgs, "").split(","))
+            .map(String::trim)
+            .collect(Collectors.toList());
+        new ToStringAgent().install(i13n, options);
     }
 
-    public void install(Instrumentation i13n) {
+    public void install(Instrumentation i13n, List<String> options) {
         new AgentBuilder.Default()
             .with(TypeStrategy.Default.REDEFINE)
-            .type(typeMatcher())
+            .type(typeMatcher(options.stream().filter(o -> !o.startsWith("--")).findFirst().orElse(null), options.contains("--force")))
             .transform(transformer())
             .installOn(i13n);
     }
 
-    private ElementMatcher<? super TypeDescription> typeMatcher() {
-        return not(declaresMethod(isToString()));
+    private ElementMatcher<? super TypeDescription> typeMatcher(String regex, boolean force) {
+        return not(force ? none() : declaresMethod(isToString()))
+            .and(regex != null ? nameMatches(regex) : any());
     }
 
     private Transformer transformer() {
